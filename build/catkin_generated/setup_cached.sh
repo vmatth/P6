5ba40e764ab8ef339c47a1671b590b58c362e0e3
vmatth <vmatth19@student.aau.dk>
2022-03-21 11:33:21 +0100

    first commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/vini/P6/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/vini/P6/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/vini/P6/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/vini/P6/src:$ROS_PACKAGE_PATH"
