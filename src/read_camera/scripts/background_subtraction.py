70f857b2b9de9156519de6ebd3eac24e51c87dff
vmatth <vmatth19@student.aau.dk>
2022-03-23 15:00:31 +0100

    Kinect calibration

--- src/read_camera/scripts/background_subtraction.py
+++ src/read_camera/scripts/background_subtraction.py
@@ -2,6 +2,7 @@
 import cv2 
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
 import rospy
 
 
@@ -10,21 +11,31 @@ class image_converter:
     def __init__(self):
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber("/kinect2/qhd/image_depth_rect", Image, self.image_callback)
-
+        self.cv_background = None
+        self.save_background = True
 
     def image_callback(self, data):
         rospy.loginfo("Receiving info from image topic!")
         try:
-            cv_image = self.bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
+            #Get image for this frame
+            cv_image = self.bridge.imgmsg_to_cv2(data, '8UC1')
+            #Normalize Image
+            cv_image_array = np.array(cv_image, dtype = np.dtype('f8'))
+            cv_image_norm = cv2.normalize(cv_image_array, cv_image_array, 0, 1, cv2.NORM_MINMAX)
+            #Get background on first frame
+            if self.save_background is True:
+                self.cv_background = cv_image_norm
+                self.save_background = False
+
+            diff = cv2.absdiff(self.cv_background, cv_image_norm)
+
+            cv2.imshow ("Background Subtraction", diff)
+            cv2.imshow("Image window", cv_image_norm)
+            cv2.imshow("Background", self.cv_background)
+            cv2.waitKey(3)
+
         except CvBridgeError as e:
             print(e)
-        
-        self.videoCapture(cv_image)
-        cv2.imshow("Image window", cv_image)
-        cv2.waitKey(3)
-
-    def videoCapture(self, data):
-        cv2.VideoCapture(0) 
 
 
 def main():
