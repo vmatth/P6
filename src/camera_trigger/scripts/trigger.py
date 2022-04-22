21ac47d61199b8abb66fa8060ea0f86c90e6e711
VinhWinz <vinhqn96@gmail.com>
2022-04-22 13:50:14 +0200

    Commit trigger.py to find contour and size

--- src/camera_trigger/scripts/trigger.py
+++ src/camera_trigger/scripts/trigger.py
@@ -23,7 +23,12 @@ class depth_dectect:
     def __init__(self):
         self.bridge = CvBridge()
         self.depth_data = None
-        #self.depth_to_parcel = None
+        self.threshold_depth = None #distances higher than this are removed
+        self.cam_height = 104.1 #cm
+
+        self.pix_per_cm = 11.4 #pix / cm ### fix so its not static
+        self.depth_list = []
+        self.counter = 0
 
     def on_press(self, key):
         try:
@@ -51,14 +56,11 @@ class depth_dectect:
         depth_data_list = []
         x_arr = []
         y_arr = []
-        pixels = []
+        lowest_pixel_depth = []
 
         lowest_depth = depth_image[0][0]
         lowest_pix = (0, 0)
-
-
-        #blur?
-        #depth_image = cv2.medianBlur(depth_image, 5)
+        list = []
 
         for y in range(0, depth_image.shape[0], 5):
             for x in range(0, depth_image.shape[1], 5):
@@ -71,32 +73,30 @@ class depth_dectect:
                         lowest_pix = (x,y)
                         print("new lowest depth found: ", lowest_depth)
                         print("at pixel: ", x, y)
-                        # x_arr.append(x)
-                        # y_arr.append(y)
-                        
-                    #pixels.append((x,y))
-                    #depth_data_list.append(depth_image[y][x])
-        cv2.circle(depth_image, lowest_pix, 3, (10000, 10000, 10000), -1)
-        #print("distance: ", depth_image[300][300])
-        #depth_min = min(depth_data_list)
-        #index_min = min(range(len(depth_data_list)), key=depth_data_list.__getitem__)
-        #min_pixel = pixels[index_min]
-        #pixels er i x,y
-       # print("Distance", depth_min)
-       # print("min pixel", min_pixel)
-       # print("index min", index_min)
-        #circle requires col, rows
+                        list.append(lowest_pix)
+                        lowest_pixel_depth.append(lowest_depth)
+    
+        # find the 4 lowest pixel coordinates
+        new_lowest_pix = list[(len(list))-4:len(list)]
+        #print("new pixel array: ", new_lowest_pix)
+        # find the 4 lowest pixel depths
+        lowest_pixel_depth = lowest_pixel_depth[(len(lowest_pixel_depth))-4:len(lowest_pixel_depth)]  
+        #print("depths: ", lowest_pixel_depth)
+ 
+        # calculate average of the 4 lowest pixel depths
+        self.threshold_depth = ((sum(lowest_pixel_depth) / len(lowest_pixel_depth))+30)/10  
+        print("thresholding depth: ", self.threshold_depth)
         #cv2.circle(depth_image, lowest_pix, 3, (10000, 10000, 10000), -1)
 
-        fig = plt.figure(figsize = (10, 7))
-        ax = plt.axes(projection ="3d")
+        # fig = plt.figure(figsize = (10, 7))
+        # ax = plt.axes(projection ="3d")
         
-        # Creating plot
-        ax.scatter3D(x_arr, y_arr, depth_data_list, color = "green")
-        plt.title("simple 3D scatter plot")
+        # # Creating plot
+        # ax.scatter3D(x_arr, y_arr, depth_data_list, color = "green")
+        # plt.title("simple 3D scatter plot")
         
-        # show plot
-        plt.show()
+        # # show plot
+        # plt.show()
         
                 
 
@@ -104,19 +104,70 @@ class depth_dectect:
     def thresholding(self, depth_data):
         try:
             depth_image = self.bridge.imgmsg_to_cv2(depth_data, "16UC1")
-            depth_image = depth_image[263:785, 635:1058]
+            depth_image = depth_image[263:785, 644:1058]
             height = depth_image.shape[0]
             width = depth_image.shape[1]
             print("height width", height, width)
 
-            
             self.get_depth(depth_image)
 
 
+            threshold_image = depth_image.copy()
+            #loop all x y pixels
+            for y in range(0,height):
+                for x in range(0, width):
+                    if depth_image[y, x] >= self.threshold_depth * 10 or depth_image[y,x] == 0:
+                        threshold_image[y, x] = 0
+                    else:
+                        threshold_image[y, x] = 10000 #10 meters (should be white)
 
+
+            converted_image = threshold_image.copy()
+            converted_image = (threshold_image/256).astype('uint8')
+
+            _, contours, _ = cv2.findContours(converted_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            print("contours :", len(contours))
+            
+
+
+            for cnt in contours:
+                area = cv2.contourArea(cnt)
+                
+                if area > 500:
+                    print("area: ", area)
+
+                    #Create a rotated box around the parcel https://theailearner.com/tag/cv2-minarearect/
+                    rect = cv2.minAreaRect(cnt)
+                    box = cv2.boxPoints(rect)
+                    box = np.int0(box)
+                    #Find centerpoint using quik maffs
+                    centerpoint_x = ((box[3][0] - box[1][0])/2) + box[1][0]
+                    centerpoint_y = ((box[0][1] - box[2][1])/2) + box[2][1]
+                    centerpoint = (centerpoint_x, centerpoint_y)
+                    center = rect[0]
+
+                    #Get pixel value at centerpoint
+                    if(centerpoint_x >= depth_image.shape[0] or centerpoint_y >= depth_image.shape[1]): #Check if all 4 corners are available, or else it returns error
+                        print("Centerpoint cannot be found in depth image")
+                    else:
+                        #Calculate dimensions & angle
+                        distance_to_parcel = (depth_image[centerpoint_x][centerpoint_y]) / 10 #Calculate distance in [cm] 
+                        #print("testtstst: ", depth_image[centerpoint_x][centerpoint_y])
+                        print("distance to parcel: ", distance_to_parcel)
+                        print("WIDTH IN PIXELS", rect[1][1])
+                        print("LENGHT IN PIXELS", rect[1][0]) 
+
+
+                    # 3d_in_cm = conversion * 2d_line * depth
+
+                    cv2.drawContours(converted_image,[box],0,(255,255,255),2)
+                    cv2.circle(converted_image, centerpoint, 3, (10000, 10000, 10000), -1)
+                    
 
 
             cv2.imshow("Raw Depth Image (*16)", depth_image * 16)
+            cv2.imshow("Threshold Image (*16)", threshold_image * 16)
+            cv2.imshow("8-bit Threshold Image", converted_image * 16)
             cv2.waitKey(0)
             #cv2.waitKey(1)
             cv2.destroyAllWindows()
